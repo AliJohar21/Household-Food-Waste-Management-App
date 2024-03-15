68d4b3f177f5b153b95348ab445c04dabf5b4636
AliJohar21 <alihamzehjohar1921@gmail.com>
2024-03-16 01:43:46 +0400

    new update

--- lib/welcome/welcome.dart
+++ lib/welcome/welcome.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:phase_2_implementation/sign_up/sign_up.dart';
+import 'package:phase_2_implementation/log_in/log_in.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
@@ -6,61 +8,96 @@ class Welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 80.0),
+        padding: const EdgeInsets.symmetric(vertical: 60.0, horizontal: 24.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'Welcome',
-              style: TextStyle(
-                fontSize: 32.0,
-                fontWeight: FontWeight.bold,
+            const SizedBox(height: 100),
+            const Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Welcome',
+                style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
               ),
             ),
-            const Text(
-              'Mohammed',
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.w400,
+            const SizedBox(height: 150),
+            Center(
+              child: Image.asset(
+                  'assets/HomePage1.png'), // Replace with your asset image path
+            ),
+            const SizedBox(height: 100),
+            const Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'ABOUT',
+                style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
               ),
             ),
-            const SizedBox(height: 50), // Adjust the size as needed
+            Text(
+              'Our aim is to reduce food waste, and ensure that food is either consumed in the right way or donated to competent authorities.',
+              style: TextStyle(fontSize: 16.0, color: Colors.grey[600]),
+            ),
+            const SizedBox(height: 40),
             Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Image.asset(
-                  'assets/HomePage.png', // Replace with your asset image path
-                  width: 150, // Adjust the size as needed
-                  height: 150, // Adjust the size as needed
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const SignUp(),
+                      ));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.purple, // Button text color
+                    ),
+                    child: const Text('Sign Up'),
+                  ),
                 ),
-                const SizedBox(
-                    width: 20), // Adjust the space between image and text
-                const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'ABOUT',
-                      style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                      ),
+                const SizedBox(width: 20), // Spacing between the buttons
+                Expanded(
+                  child: OutlinedButton(
+                    onPressed: () {
+                      //Navigate to login screen
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const LogIn()),
+                      );
+                    },
+                    style: OutlinedButton.styleFrom(
+                      foregroundColor: Colors.purple,
+                      side: const BorderSide(
+                          color: Colors.purple), // Border color
                     ),
-                    SizedBox(height: 10), // Adjust the size as needed
-                    Text(
-                      'Our aim is to reduce food waste, and ensure that food is either consumed in the right way or donated to competent authorities.',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
+                    child: const Text('Login'),
+                  ),
                 ),
               ],
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        backgroundColor: Colors.purple,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white70,
       ),
     );
   }
