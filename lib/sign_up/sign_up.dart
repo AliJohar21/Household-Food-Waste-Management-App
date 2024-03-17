2d9a72a08dcc90b6a2b50230b9cf91b718dbb7cd
AliJohar21 <alihamzehjohar1921@gmail.com>
2024-03-17 15:19:05 +0400

    new

--- lib/sign_up/sign_up.dart
+++ lib/sign_up/sign_up.dart
@@ -2,128 +2,168 @@ import 'package:flutter/material.dart';
 import 'package:phase_2_implementation/log_in/log_in.dart';
 
 class SignUp extends StatefulWidget {
-  const SignUp({
-    super.key,
-  });
+  const SignUp({super.key});
 
   @override
-  State<SignUp> createState() => _LogInState();
+  _SignUpState createState() => _SignUpState();
 }
 
-class _LogInState extends State<SignUp> {
-  var hideText = true;
+class _SignUpState extends State<SignUp> {
+  final _formKey = GlobalKey<FormState>();
+  var hidePassword = true;
+  var hideConfirmPassword = true;
   final nameController = TextEditingController();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
+  bool isEmail(String input) =>
+      RegExp(r'\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b').hasMatch(input);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          // Background image
           Container(
             width: double.infinity,
             height: double.infinity,
             decoration: const BoxDecoration(
               image: DecorationImage(
-                image: AssetImage(
-                    'assets/b_g.png'), // Path to your background image
-                fit: BoxFit.cover, // Adjust the fit as per your requirement
+                image: AssetImage('assets/b_g.png'),
+                fit: BoxFit.cover,
               ),
             ),
           ),
-          // Login UI
           Center(
             child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-              ),
-              child: SizedBox(
-                height: 500,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    const Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 20), // Space above "Sign Up" text
+                      const Text(
                         "Sign Up",
                         style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 20), // Additional spacing
+                      TextFormField(
+                        controller: nameController,
+                        decoration: const InputDecoration(
+                          labelText: "Full Name",
+                          border: OutlineInputBorder(),
                         ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your name';
+                          }
+                          return null;
+                        },
                       ),
-                    ),
-                    TextField(
-                      controller: nameController,
-                      decoration: const InputDecoration(
-                        labelText: "Full Name",
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    TextField(
-                      controller: emailController,
-                      decoration: const InputDecoration(
-                        labelText: "E-Mail",
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    TextField(
-                      controller: passwordController,
-                      obscureText: hideText,
-                      decoration: InputDecoration(
-                        suffixIcon: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              hideText = !hideText;
-                            });
-                          },
-                          icon: hideText
-                              ? const Icon(Icons.visibility_off)
-                              : const Icon(Icons.visibility),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: emailController,
+                        decoration: const InputDecoration(
+                          labelText: "E-Mail",
+                          border: OutlineInputBorder(),
                         ),
-                        labelText: "Password",
-                        border: const OutlineInputBorder(),
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              !isEmail(value)) {
+                            return 'Please enter a valid email';
+                          }
+                          return null;
+                        },
                       ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        //login to firebase
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LogIn()),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.deepPurple, // text color
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: passwordController,
+                        obscureText: hidePassword,
+                        decoration: InputDecoration(
+                          suffixIcon: IconButton(
+                            onPressed: () =>
+                                setState(() => hidePassword = !hidePassword),
+                            icon: Icon(hidePassword
+                                ? Icons.visibility_off
+                                : Icons.visibility),
+                          ),
+                          labelText: "Password",
+                          border: const OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              value.length < 6) {
+                            return 'Password must be at least 6 characters long';
+                          }
+                          return null;
+                        },
                       ),
-                      child: const Padding(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 40, vertical: 12),
-                        child: Text("SIGN UP"),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: confirmPasswordController,
+                        obscureText: hideConfirmPassword,
+                        decoration: InputDecoration(
+                          suffixIcon: IconButton(
+                            onPressed: () => setState(() =>
+                                hideConfirmPassword = !hideConfirmPassword),
+                            icon: Icon(hideConfirmPassword
+                                ? Icons.visibility_off
+                                : Icons.visibility),
+                          ),
+                          labelText: "Confirm Password",
+                          border: const OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value != passwordController.text) {
+                            return 'Passwords do not match';
+                          }
+                          return null;
+                        },
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text("Already Have an account?"),
-                        const SizedBox(width: 8),
-                        TextButton(
-                          onPressed: () {
-                            //Navigate to login screen
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const LogIn()),
+                      const SizedBox(
+                          height: 20), // Space before the "SIGN UP" button
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Processing Data')),
                             );
-                          },
-                          child: const Text("Login"),
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const LogIn()));
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.deepPurple,
+                          minimumSize: const Size(150, 40),
                         ),
-                      ],
-                    ),
-                  ],
+                        child: const Text("SIGN UP"),
+                      ),
+                      const SizedBox(
+                          height: 20), // Space after "SIGN UP" button
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Already Have an account?"),
+                          TextButton(
+                            onPressed: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const LogIn())),
+                            child: const Text("Login"),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
